d2318bb1aae046120707dc54c563fd53d4f68312
DolinAlexey <dolin.alexey@gmail.com>
2024-01-22 17:06:11 +0300

    Add files via upload

--- src/get.rs
+++ src/get.rs
@@ -10,8 +10,6 @@ pub mod functions {
         }
     }
 
-
-
     //Принимает мутабельную ссылку на слайс и число N. Возвращает мутабельную ссылку на N-ый элемент.
     pub fn get_mutable_reference_slice<T>(slice: &mut [T], n: usize) -> &mut T {
         &mut slice[n]
@@ -23,32 +21,29 @@ pub mod functions {
     }
 
     //Принимает слайс и число N. Возвращает два слайса с элементами:
-// с нулевого по N-1;
-// с N-го по последний;
+    // с нулевого по N-1;
+    // с N-го по последний;
     pub fn get_slice_and_number_n_return_two_slides<T>(slice: &[T], n: usize) -> (&[T], &[T]) {
-        let (from_zero_to_n_minus_1, from_nth_to_last) = slice.split_at(n);
-        (from_zero_to_n_minus_1, from_nth_to_last)
+        slice.split_at(n)
     }
 
     //Принимает слайс и возвращает массив слайсов, содержащий четыре равные (насколько возможно) части исходного слайса.
-// Протестировать функции.
-// Убедиться, что копилятор не позволит вернуть более одной мутабельной ссылки на один объект.
+    // Протестировать функции.
+    // Убедиться, что копилятор не позволит вернуть более одной мутабельной ссылки на один объект.
     pub fn get_slice_and_return_four_equal_parts<T>(slice: &[T]) -> [&[T]; 4] {
         let len = slice.len();
-        let fragment_size = len / 4;
-        let mut result = [&[][..]; 4];
+        let fragment_size = (len + 3) / 4;
+        let mut result: [&[T]; 4] = Default::default();
 
         let mut start = 0;
-        for (i, fragment) in result.iter_mut().enumerate() {
-            let remaining = len - start;
-            let size = if i < 3 { fragment_size } else { remaining };
-            let end = start + size;
-            let slice_fragment = &slice[start..end];
-            *fragment = slice_fragment;
-            start = end;
+        for fragment in result.iter_mut() {
+            *fragment = &slice[start..(start + fragment_size).min(len)];
+            start += fragment_size;
         }
         result
     }
+
+
     #[test]
     fn test_get_mutable_reference_tuple() {
         let mut tuple = (11, 48);
@@ -82,11 +77,11 @@ pub mod functions {
 
     #[test]
     fn test_get_slice_and_return_four_equal_parts() {
-        let slice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+        let slice = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
         let _result = get_slice_and_return_four_equal_parts(&slice);
-        assert_eq!(_result[0], &[1, 2]);
-        assert_eq!(_result[1], &[3, 4]);
-        assert_eq!(_result[2], &[5, 6]);
-        assert_eq!(_result[3], &[7, 8, 9, 10, 11]);
+        assert_eq!(_result[0], &[1, 2, 3, 4]);
+        assert_eq!(_result[1], &[5, 6, 7, 8]);
+        assert_eq!(_result[2], &[9, 10, 11, 12]);
+        assert_eq!(_result[3], &[13, 14]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,13 @@ fn main() {
     let slice = [11, 22, 33, 44, 55];
     let (_left, _right) = get_slice_and_number_n_return_two_slides(&slice, 3);
 
-    let slice = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100, 200, 300];
+    let slice = [11, 22, 33, 44, 55, 66, 77, 88, 99, 100];
     let _result = get_slice_and_return_four_equal_parts(&slice);
 
-    let slice = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    let slice = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
     let _result = get_slice_and_return_four_equal_parts(&slice);
 
     // Убедиться, что копилятор не позволит вернуть более одной мутабельной ссылки на один объект.
     let (_left, _right) = get_slice_and_number_n_return_two_slides(&slice, 2);
     //let (_left1, _left2) = get_slice_and_number_n_return_two_slides(&mut slice, 1);
+}
